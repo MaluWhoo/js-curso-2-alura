12481c717a46388ec14eab764adac8b5fc77a8f9
Maria Luiza Lima <luizagamesl90@gmail.com>
2025-10-04 16:25:02 -0300

    Files

---
+++ app.js
@@ -0,0 +1,7 @@
+let titulo = document.querySelector('h1');
+// innerHTML --> A propriedade innerHTML é usada para obter ou definir o conteúdo HTML de um elemento no DOM.
+titulo.innerHTML = 'Jogo do número secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
