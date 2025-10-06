012cc0b4b20d9667a1ccba01e3864887e6e1192e
Maria Luiza Lima <luizagamesl90@gmail.com>
2025-10-06 12:42:25 -0300

    Update js

--- app.js
+++ app.js
@@ -5,3 +5,6 @@ titulo.innerHTML = 'Jogo do número secreto';
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
+function verificarChute(){
+    console.log("botao clicado");
+}
